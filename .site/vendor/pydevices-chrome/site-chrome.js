9982de00af9bc94ea32e1fbbe9f8fa0739c2031b
Brad Barnett <bbarnett@gmail.com>
2026-08-14 00:17:30 -0500

    chore: standardize documentation and workflows to mip repository

--- .site/vendor/pydevices-chrome/site-chrome.js
+++ .site/vendor/pydevices-chrome/site-chrome.js
@@ -13,7 +13,7 @@
  *   https://pydevices.github.io/assets/js/site-chrome.js
  */
 (function () {
-  var LOGO = "https://pydevices.github.io/assets/img/logo.svg";
+  var LOGO = "https://pydevices.github.io/img/logo.svg";
   var ROOT = "https://pydevices.github.io";
 
   var HEADER =
@@ -111,7 +111,7 @@
     '<div class="tree-branch open">' +
     '<div class="tree-branch-header"><svg class="tree-toggle-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M9 18l6-6-6-6"/></svg> 📁 5: Developer Tools & Infrastructure</div>' +
     '<div class="tree-branch-children">' +
-    '<a class="tree-leaf" href="https://pydevices.github.io/micropython-lib/"><span class="tree-leaf-name">micropython-lib</span><span class="tag tag-tier-5">PyDevices MIP Index</span></a>' +
+    '<a class="tree-leaf" href="https://pydevices.github.io/mip/"><span class="tree-leaf-name">mip</span><span class="tag tag-tier-5">PyDevices MIP Index</span></a>' +
     '<a class="tree-leaf" href="https://pydevices.github.io/cmods/"><span class="tree-leaf-name">cmods</span><span class="tag tag-tier-5">Build Workspace</span></a>' +
     '<a class="tree-leaf" href="https://pydevices.github.io/mpftp/"><span class="tree-leaf-name">mpftp</span><span class="tag tag-tier-5">REPL / Transfer Tool</span></a>' +
     '</div>' +
@@ -136,7 +136,7 @@
     }
     if (!document.querySelector('script[src*="tree-nav.js"]')) {
       var script = document.createElement('script');
-      script.src = ROOT + '/assets/js/tree-nav.js';
+      script.src = ROOT + '/vendor/pydevices-chrome/tree-nav.js';
       document.head.appendChild(script);
     }
   }
